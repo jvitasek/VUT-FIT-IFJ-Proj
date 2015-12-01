1b9f44be43bb6161595c0864bcd9c1bb72e522f1
MVyroubal <xvyrou05@stud.fit.vutbr.cz>
2015-12-01 16:09:37 +0100

    instrukce, struktura operandu

--- ilist.h
+++ ilist.h
@@ -14,17 +14,17 @@
 #ifndef ILIST_H_
 #define ILIST_H_
 
-/*
+/**
  * Typy instrukci
  */
 typedef enum {
- 	/* Aritmeticke operace */
+ 	// aritmeticke operace 
 	I_ADD, // (dest, addr, addr) +
 	I_SUB, // (dest, addr, addr) -
 	I_MUL, // (dest, addr, addr) *
 	I_DIV, // (dest, addr, addr) /
 
-	/* relacni operace*/
+	// relacni operace
 	I_EQ, // (dest, addr, addr) ==
 	I_NEQ, // (dest, addr, addr) !=
 	I_LE, // (dest, addr, addr) <
@@ -33,44 +33,27 @@ typedef enum {
 	I_GEQ, // (dest, addr, addr) >=
 	I_NEG, // (dest, addr, - ) 
 
-	/* prirazeni */
+	// prirazeni
 	I_ASSIGN, // (dest, addr, - )
 
-	/* instrukce skoku */
+	// instrukce skoku
 	I_GOTO, // (adr, - , - ) skoci na adresu adr 
 	I_IFGOTO, // (adr, cond, - ) skoci na adresu adr, pokud plati podminka cond
-	
 
-	/* zasobnik parametru*/
+	// zasobnik parametru
 	I_PUSH, // pushne na zasobnik
 	I_POP, // odstrani a vrati hodnotu z vrcholu zasobniku
 	I_NOP,  // prazdna operace
 
 	I_STOP, //konec programu
 
-	/*
-	* Pokud interpret narazi na instrukci I_FSKIP, preskoci vsechny instrukce
-	* az po instrukci I_FSTOP
-	*/
-	I_FSKIP, // (-,-,-) => nema operandy
-	I_FSTOP, // zarazka pro instrukci I_FSKIP: (-,-,-)
-
-	/*
-	* Return
-	*/
+	// return
 	I_RETURN, // (-, -, -)
 
-	/*
-	* Preda rizeni volane funkci
-	*/
-	I_CALL, // (id_funkce, adresa_kam_se_vynori, - )
+	// preda rizeni volane funkci
+	I_CALL, // (id_funkce, addr, - )
 
-	/* Navesti */
-	I_LABEL, // navesti urcene pro skoky (priznak, -, -)
-
-	/*
-	* Instrunkce pro vestavene funkce
-	*/
+	// vestavene funkce
 	I_LENGTH,
 	I_SUBSTR,
 	I_CONCAT,
@@ -78,28 +61,46 @@ typedef enum {
 	I_SORT 
 } tInstCode;
 
-/*
+/**
+ * Struktura operandu
+ */
+typedef struct operand
+{
+	TypeI type; // typ operandu
+	void *obsah // obsah promenne/operandu
+};
+
+/**
+ * Datovy typ
+ */
+typedef enum
+{
+	INT, 	
+	DOUBLE, 
+	STRING
+} TypeI;
+
+/**
  * Struktura reprezentujici 3AC instrukci
  */
 typedef struct Inst 
 {
 	tInstCode instType; // typ instrukce
-	void *op1; // operand 1
-	void *op2; // operand 2
-	void *res; // result
+	operand *op1; // operand 1
+	operand *op2; // operand 2
+	operand *res; // result
 } tInst;
 
-/*
+/**
  * Polozka seznamu, ktera obsahuje konkretni instrukci
  */
 typedef struct ListItem 
 {
 	tInst Instruction;
 	struct ListItem *NextItem;
-
 } tListItem;
 
-/*
+/**
  * Struktura reprezentujici seznam instrukci, aktivni prvek predstavuje aktualni instrukci
  */
 typedef struct ListOfInstr 
